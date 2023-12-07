5e2c8f7621fb5db9fb54c4c2167c86824c2d35bd
Роман Чалый <r.chaliy@corp.mail.ru>
2023-12-07 11:02:13 +0300

    test(benchmark): add benchmark to show difference between canonical headers and non-canonical one.

--- analyzer_test.go
+++ analyzer_test.go
@@ -1,12 +1,15 @@
 package canonicalheader_test
 
 import (
+	"net/http"
 	"testing"
 
 	"golang.org/x/tools/go/analysis/analysistest"
 
 	"github.com/lasiar/canonicalheader"
 )
+
+const testValue = "hello_world"
 
 func TestAnalyzer(t *testing.T) {
 	t.Parallel()
@@ -16,4 +19,32 @@ func TestAnalyzer(t *testing.T) {
 		canonicalheader.Analyzer,
 		"p",
 	)
+}
+
+func BenchmarkCanonical(b *testing.B) {
+	v := http.Header{
+		"Canonical-Header": []string{testValue},
+	}
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		s := v.Get("Canonical-Header")
+		if s != testValue {
+			b.Fatal()
+		}
+	}
+}
+
+func BenchmarkNonCanonical(b *testing.B) {
+	v := http.Header{
+		"Canonical-Header": []string{testValue},
+	}
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		s := v.Get("CANONICAL-HEADER")
+		if s != testValue {
+			b.Fatal()
+		}
+	}
 }
