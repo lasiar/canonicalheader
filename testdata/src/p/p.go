03f6e8abf09aa3b264a73db1e78462f51cef65f1
Roman Chaliy <cerebrum.ch@gmail.com>
2024-04-23 00:34:14 +0700

    test: add raw string literal

--- testdata/src/p/p.go
+++ testdata/src/p/p.go
@@ -10,11 +10,12 @@ func p() {
 	v := http.Header{}
 	v.Get(testHeader)
 
-	v.Get("Test-HEader")          // want `non-canonical header "Test-HEader", instead use: "Test-Header"`
-	v.Set("Test-HEader", "value") // want `non-canonical header "Test-HEader", instead use: "Test-Header"`
-	v.Add("Test-HEader", "value") // want `non-canonical header "Test-HEader", instead use: "Test-Header"`
-	v.Del("Test-HEader")          // want `non-canonical header "Test-HEader", instead use: "Test-Header"`
-	v.Values("Test-HEader")       // want `non-canonical header "Test-HEader", instead use: "Test-Header"`
+	v.Get("Test-HEader")           // want `non-canonical header "Test-HEader", instead use: "Test-Header"`
+	v.Set("Test-HEader", "value")  // want `non-canonical header "Test-HEader", instead use: "Test-Header"`
+	v.Add("Test-HEader", "value")  // want `non-canonical header "Test-HEader", instead use: "Test-Header"`
+	v.Del("Test-HEader")           // want `non-canonical header "Test-HEader", instead use: "Test-Header"`
+	v.Values("Test-HEader")        // want `non-canonical header "Test-HEader", instead use: "Test-Header"`
+	v.Values(`Raw-STRING-Literal`) // want `non-canonical header "Raw-STRING-Literal", instead use: "Raw-String-Literal"`
 
 	v.Set("Test-Header", "value")
 	v.Add("Test-Header", "value")
